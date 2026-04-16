f2ebabe15d88cbdad88420f9048e1486b1e4b699
Rahul Gupta <rahul.gupta@freecharge.com>
2026-04-17 01:06:58 +0530

    updated config of vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      pako: path.resolve(__dirname, "./node_modules/browserify-zlib/node_modules/pako"),
     },
   },
-}));
+});
