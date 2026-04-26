28e35343315b68cefcd87f4acf1e4745fec53335
Rohini Rani <rohini.rani@freecharge.com>
2026-04-26 23:54:36 +0530

    phone number updating on checkout with +91

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -308,6 +308,7 @@ export default function CheckoutPage() {
       const isd = profileDetails.isdCode;
       const payload = {
         ...profileDetails,
+        phoneNumber: phoneNumber,
         isdCode: isd,
         ...(!profileDetails.emailId && { emailId }),
         ...(!profileDetails.firstName && { firstName: shippingFirstName }),
